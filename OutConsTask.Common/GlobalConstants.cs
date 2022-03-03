3461310da57a7abbb9efb2e0215a6b8c2d70aa68
GeorgiGichev <georgi.gichev87@gmail.com>
2022-03-03 23:30:57 +0200

    deleting needless files

--- OutConsTask.Common/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OutConsTask.Common
-{
-    public static class GlobalConstants
-    {
-        public const string SystemName = "OutConsTask";
-
-        public const string AdministratorRoleName = "Administrator";
-    }
-}
